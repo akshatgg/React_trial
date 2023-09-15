1db1104ee90b953cdd84dd0523b3cd64c632bf02
akshatgg <akshatg9636@gmail.com>
2023-09-16 01:59:44 +0545

    first-app

---
+++ first-app/src/App.js
@@ -0,0 +1,16 @@
+
+import DogCard from './DogCard';
+function App() {
+  return (
+    <div className="App">
+    hello
+    <DogCard />
+    <DogCard />
+    <DogCard />
+    <DogCard />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ first-app/src/DogCard.js
@@ -0,0 +1,13 @@
+function image(){
+    <img src="https://media.istockphoto.com/id/513133900/photo/golden-retriever-sitting-in-front-of-a-white-background.jpg?s=612x612&w=0&k=20&c=rPuBgfn_wcAzaa8o2GhrA2eBTdbvrTvYw4demzV-bOs="/>
+
+}
+function DogCard(){
+    return(
+<div>
+    <h3>Bruno</h3>
+    <image />
+</div>
+    );
+}
+export default DogCard;
