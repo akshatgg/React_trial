eebf3f8c3d705914df747bcc0449c6e0177a632e
akshatgg <akshatg9636@gmail.com>
2023-11-02 20:34:41 +0545

    first app modifyy

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
     hello
     <DogCard name="brunoo" Image="https://media.istockphoto.com/id/962855368/photo/beagle-5-years-old-sitting-in-front-of-white-background.jpg?s=612x612&w=0&k=20&c=LSDUgTw68-FNU43TmLQA1_e46lew9iBpGvaJdFB-B1s="/>
-    <DogCard name="brunoo" Image="https://media.istockphoto.com/id/962855368/photo/beagle-5-years-old-sitting-in-front-of-white-background.jpg?s=612x612&w=0&k=20&c=LSDUgTw68-FNU43TmLQA1_e46lew9iBpGvaJdFB-B1s="/>
+    <DogCard  Image="https://media.istockphoto.com/id/962855368/photo/beagle-5-years-old-sitting-in-front-of-white-background.jpg?s=612x612&w=0&k=20&c=LSDUgTw68-FNU43TmLQA1_e46lew9iBpGvaJdFB-B1s="/>
 
     <DogCard name="brun" Image="https://media.istockphoto.com/id/962855368/photo/beagle-5-years-old-sitting-in-front-of-white-background.jpg?s=612x612&w=0&k=20&c=LSDUgTw68-FNU43TmLQA1_e46lew9iBpGvaJdFB-B1s="/>
     </div>
--- first-app/src/DogCard.js
+++ first-app/src/DogCard.js
@@ -2,8 +2,8 @@ import Image from "./Image";
 import Name from "./Name";
 // function Image(){
 //     return <img src="https://media.istockphoto.com/id/513133900/photo/golden-retriever-sitting-in-front-of-a-white-background.jpg?s=612x612&w=0&k=20&c=rPuBgfn_wcAzaa8o2GhrA2eBTdbvrTvYw4demzV-bOs="/>;
-
 // }
+
 function DogCard(props){
     let title="hi i am akshat";
     return(
