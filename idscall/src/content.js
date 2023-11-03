5c32bae6756eca32976c16a52f6bc86975ce10ea
akshatgg <akshatg9636@gmail.com>
2023-11-04 01:56:19 +0545

    trash btn error element not vanished

--- idscall/src/content.js
+++ idscall/src/content.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FaShieldAlt, FaTrashAlt } from 'react-icons/fa';
-
+import './content.css'
 function Content({ updateBasketLength }) {
   const [items, setItems] = useState([]);
   const [basket, setBasket] = useState([]);
@@ -36,6 +36,18 @@ function Content({ updateBasketLength }) {
     }
   };
 
+
+const remove=document.querySelector("#element");
+
+const ElementDisplay=(id)=>{
+
+  const newBaskets = basket.filter((item) => item.id !== id);
+  remove.style.display="none";
+  setBasket(newBaskets);
+    storeBasket(newBaskets);
+    updateBasketLength(newBaskets.length);
+}
+
   const removeFromBasket = (id) => {
     const newBasket = basket.filter((item) => item.id !== id);
     setBasket(newBasket);
@@ -70,11 +82,11 @@ function Content({ updateBasketLength }) {
 
 
   return (
-    <div className="flex items-center my-4 justify-center">
+    <div className="flex items-center my-4 justify-center ">
     <div className="text-2xl flex items-center  justify-center h-44 border w-4/12  ">
       <ul>
         {items.map((item) => (
-          <li className="item" key={item.id}>
+          <li id="element" className="item bg-gray-100 pl-9  pr-8 px-2 py-2 my-1 border border-gray-500" key={item.id} style={{ border: '1px solid #000' }}>
             <div className="flex space-x-1.5">
               <input
                 type="checkbox"
@@ -85,7 +97,10 @@ function Content({ updateBasketLength }) {
               <FaTrashAlt
                 role="button"
                 tabIndex="0"
-                onClick={() => removeFromBasket(item.id)}
+                onClick={() => 
+                  // removeFromBasket(item.id)
+                  ElementDisplay(item.id)
+                }
               />
             </div>
           </li>
