5ee51a3f74123de36a894cd5ecedf0620e0c2881
akshatgg <akshatg9636@gmail.com>
2023-11-05 12:43:45 +0545

    error comes to hide block

--- idscall/src/content.js
+++ idscall/src/content.js
@@ -1,6 +1,7 @@
+/* eslint-disable array-callback-return */
 import React, { useState, useEffect } from 'react';
-import { FaShieldAlt, FaTrashAlt } from 'react-icons/fa';
-import './content.css'
+import { FaTrashAlt } from 'react-icons/fa';
+
 function Content({ updateBasketLength }) {
   const [items, setItems] = useState([]);
   const [basket, setBasket] = useState([]);
@@ -14,7 +15,7 @@ function Content({ updateBasketLength }) {
     const newBasket = [...basket, { id, checked, item, price }];
     setBasket(newBasket);
     storeBasket(newBasket);
-    updateBasketLength(newBasket.length); // Notify App component
+    updateBasketLength(newBasket.length);
   };
 
   const storeBasket = (basketToStore) => {
@@ -36,25 +37,22 @@ function Content({ updateBasketLength }) {
     }
   };
 
-
-const remove=document.querySelector("#element");
-
-const ElementDisplay=(id)=>{
-
-  const newBaskets = basket.filter((item) => item.id !== id);
-  remove.style.display="none";
-  setBasket(newBaskets);
-    storeBasket(newBaskets);
-    updateBasketLength(newBaskets.length);
-}
-
   const removeFromBasket = (id) => {
     const newBasket = basket.filter((item) => item.id !== id);
     setBasket(newBasket);
     storeBasket(newBasket);
-    updateBasketLength(newBasket.length); // Notify App component
+    updateBasketLength(newBasket.length);
   };
+const product=document.querySelector('#item')
+const RemoveElement=(id)=>{
+  const update=items.map((item)=>{
 
+if(item.id === id){
+    product.style.display='none'
+}
+setItems(update);
+  })
+}
   useEffect(() => {
     setItems([
       {
@@ -78,35 +76,29 @@ const ElementDisplay=(id)=>{
     ]);
   }, []);
 
-
-
-
   return (
-    <div className="flex items-center my-4 justify-center ">
-    <div className="text-2xl flex items-center  justify-center h-44 border w-4/12  ">
-      <ul>
+    <div className="flex items-center my-4 justify-center">
+      <div className="text-2xl flex items-center justify-center h-44 border w-4/12">
+        <ul>
         {items.map((item) => (
-          <li id="element" className="item bg-gray-100 pl-9  pr-8 px-2 py-2 my-1 border border-gray-500" key={item.id} style={{ border: '1px solid #000' }}>
-            <div className="flex space-x-1.5">
-              <input
-                type="checkbox"
-                onClick={() => toggleItem(item.id)}
-                checked={item.checked}
-              />
-              <label onClick={() => toggleItem(item.id)}>{item.item}</label>
-              <FaTrashAlt
-                role="button"
-                tabIndex="0"
-                onClick={() => 
-                  // removeFromBasket(item.id)
-                  ElementDisplay(item.id)
-                }
-              />
-            </div>
-          </li>
-        ))}
-      </ul>
+  <li id={`item-${item.id}`} className="item bg-gray-100 pl-9 pr-8 px-2 py-2 my-1 border border-gray-500">
+    <div className="flex space-x-1.5">
+      <input
+        type="checkbox"
+        onClick={() => toggleItem(item.id)}
+        checked={item.checked}
+      />
+      <label onClick={() => toggleItem(item.id)}>{item.item}</label>
+      <FaTrashAlt
+        role="button"
+        tabIndex="0"
+        onClick={() => RemoveElement(item.id)}
+      />
     </div>
+  </li> 
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
