1bedf25c0bd0c65f8f4d8cb048e906e8d29b2049
akshatgg <akshatg9636@gmail.com>
2023-11-05 12:45:20 +0545

    error fix to gide the element block

--- idscall/src/content.js
+++ idscall/src/content.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { FaShieldAlt, FaTrashAlt } from 'react-icons/fa';
-import './content.css'
+import { FaTrashAlt } from 'react-icons/fa';
+
 function Content({ updateBasketLength }) {
   const [items, setItems] = useState([]);
   const [basket, setBasket] = useState([]);
@@ -14,7 +14,7 @@ function Content({ updateBasketLength }) {
     const newBasket = [...basket, { id, checked, item, price }];
     setBasket(newBasket);
     storeBasket(newBasket);
-    updateBasketLength(newBasket.length); // Notify App component
+    updateBasketLength(newBasket.length);
   };
 
   const storeBasket = (basketToStore) => {
@@ -36,23 +36,18 @@ function Content({ updateBasketLength }) {
     }
   };
 
-
-const remove=document.querySelector("#element");
-
-const ElementDisplay=(id)=>{
-
-  const newBaskets = basket.filter((item) => item.id !== id);
-  remove.style.display="none";
-  setBasket(newBaskets);
-    storeBasket(newBaskets);
-    updateBasketLength(newBaskets.length);
-}
+  const hideItem = (id) => {
+    const updatedItems = items.map((item) =>
+      item.id === id ? { ...item, hidden: true } : item
+    );
+    setItems(updatedItems);
+  };
 
   const removeFromBasket = (id) => {
     const newBasket = basket.filter((item) => item.id !== id);
     setBasket(newBasket);
     storeBasket(newBasket);
-    updateBasketLength(newBasket.length); // Notify App component
+    updateBasketLength(newBasket.length);
   };
 
   useEffect(() => {
@@ -60,53 +55,52 @@ const ElementDisplay=(id)=>{
       {
         id: 1,
         checked: false,
+        hidden: false,
         item: "Old Mong",
         price: 1200,
       },
       {
         id: 2,
         checked: false,
+        hidden: false,
         item: "Blue Lagoon",
         price: 450,
       },
       {
         id: 3,
         checked: false,
+        hidden: false,
         item: "Buttvizer",
         price: 200,
       },
     ]);
   }, []);
 
-
-
-
   return (
-    <div className="flex items-center my-4 justify-center ">
-    <div className="text-2xl flex items-center  justify-center h-44 border w-4/12  ">
-      <ul>
-        {items.map((item) => (
-          <li id="element" className="item bg-gray-100 pl-9  pr-8 px-2 py-2 my-1 border border-gray-500" key={item.id} style={{ border: '1px solid #000' }}>
-            <div className="flex space-x-1.5">
-              <input
-                type="checkbox"
-                onClick={() => toggleItem(item.id)}
-                checked={item.checked}
-              />
-              <label onClick={() => toggleItem(item.id)}>{item.item}</label>
-              <FaTrashAlt
-                role="button"
-                tabIndex="0"
-                onClick={() => 
-                  // removeFromBasket(item.id)
-                  ElementDisplay(item.id)
-                }
-              />
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <div className="flex items-center my-4 justify-center">
+      <div className="text-2xl flex items-center justify-center h-44 border w-4/12">
+        <ul>
+          {items
+            .filter((item) => !item.hidden)
+            .map((item) => (
+              <li key={`item-${item.id}`} className="item bg-gray-100 pl-9 pr-8 px-2 py-2 my-1 border border-gray-500">
+                <div className="flex space-x-1.5">
+                  <input
+                    type="checkbox"
+                    onClick={() => toggleItem(item.id)}
+                    checked={item.checked}
+                  />
+                  <label onClick={() => toggleItem(item.id)}>{item.item}</label>
+                  <FaTrashAlt
+                    role="button"
+                    tabIndex="0"
+                    onClick={() => hideItem(item.id)}
+                  />
+                </div>
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 }
