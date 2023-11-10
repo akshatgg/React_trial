454b1d8ac7cab9ab577d2491ab6cf5765c25b969
akshatgg <akshatg9636@gmail.com>
2023-11-11 01:03:09 +0545

    removing unessary things

--- Grocery List/src/Content.js
+++ Grocery List/src/Content.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import ItemList from './ItemList';
 
 function Content({ updateBasketLength,items,toggleItem,HideandRemove}) {
